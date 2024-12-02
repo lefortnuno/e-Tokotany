8d1ff4591b929c707f353e7a741fd5d40f122de9
lefortnuno <trofelnuno@gmail.com>
2024-12-02 09:18:02 +0100

    Glitch Login

--- backend/routes/bureau.route.js
+++ backend/routes/bureau.route.js
@@ -5,11 +5,12 @@ const client = require("../middlewares/client.middleware");
 router.post("/", client.checkUtilisateur, BureauController.addBureau);
 
 router.get("/", client.checkUtilisateur, BureauController.getAllBureau);
+router.get("/glitch/", BureauController.getAllBureau);
 router.get("/:id", client.checkUtilisateur, BureauController.getIdBureau);
 router.get(
-	"/recherche/:valeur",
-	client.checkUtilisateur,
-	BureauController.searchBureau
+  "/recherche/:valeur",
+  client.checkUtilisateur,
+  BureauController.searchBureau
 );
 
 router.put("/:id", client.checkUtilisateur, BureauController.updateBureau);
--- frontend/src/components/login/SeConnecter.js
+++ frontend/src/components/login/SeConnecter.js
@@ -2,34 +2,51 @@ import FormulaireSeConnecter from "./Form.SeConnecter";
 import HeaderContext from "../../contexts/header/header.context";
 import SidebarContext from "../../contexts/sidebar/sidebar.context";
 import FooterContext from "../../contexts/footer/footer.context";
-// import { AjoutLibrary, libraryList } from "../../api/file.js";
+import axios from "../../api/axios"; 
+import { useEffect } from "react";
 
 export default function SeConnecter() {
-	return (
-		<>
-			<div className="wrapper">
-				<HeaderContext />
-				<SidebarContext />
+  useEffect(() => {
+    const fetchData = () => {
+      axios
+        .get("bureau/glitch/")
+        .then(function (response) {
+        //   console.log("Données récupérées : ", response.data);
+        })
+        .catch((error) => {
+        //   console.error("Erreur lors de la récupération : ", error);
+        });
+    };
 
-				<div className="main-panel">
-					<div className="container-login100">
-						<div className="wrap-login100">
-							<div className="login100-pic js-tilt" data-tilt>
-								<img
-									src={process.env.PUBLIC_URL + `/picture/logo/fanjakana.jpg`}
-									alt="image"
-									// style={{ width: "100%", height: "80%", borderRadius: "0%" }}
-									style={{ marginTop: "16%" }}
-								/>
-							</div>
+    fetchData();
+    const interval = setInterval(fetchData, 60000); // 1.MIN
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <>
+      <div className="wrapper">
+        <HeaderContext />
+        <SidebarContext />
 
-							<FormulaireSeConnecter />
-						</div>
-					</div>
-					<FooterContext />
-				</div>
-			</div>
-			{/* {libraryList.forEach((x) => AjoutLibrary(x))} */}
-		</>
-	);
+        <div className="main-panel">
+          <div className="container-login100">
+            <div className="wrap-login100">
+              <div className="login100-pic js-tilt" data-tilt>
+                <img
+                  src={process.env.PUBLIC_URL + `/picture/logo/fanjakana.jpg`}
+                  alt="image"
+                  // style={{ width: "100%", height: "80%", borderRadius: "0%" }}
+                  style={{ marginTop: "16%" }}
+                />
+              </div>
+
+              <FormulaireSeConnecter />
+            </div>
+          </div>
+          <FooterContext />
+        </div>
+      </div>
+      {/* {libraryList.forEach((x) => AjoutLibrary(x))} */}
+    </>
+  );
 }
